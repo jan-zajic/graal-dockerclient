1a897e467e645e0f31714afdf8238e8e5566f185
jzajic <jan.zajic@corpus.cz>
2018-11-17 02:22:00 +0100

    debug print

--- src/main/java/net/jzajic/graalvm/client/EntityHandler.java
+++ src/main/java/net/jzajic/graalvm/client/EntityHandler.java
@@ -2,6 +2,7 @@ package net.jzajic.graalvm.client;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.client.ResponseHandler;
@@ -10,6 +11,7 @@ import org.apache.http.impl.io.EmptyInputStream;
 
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.io.ByteStreams;
 
 @FunctionalInterface
 public interface EntityHandler<T> {
@@ -34,17 +36,24 @@ public interface EntityHandler<T> {
   	return handler.asResponseHandler();
   }
   
+  static final boolean DEBUG = true;
+  
   static <T> ResponseHandler<T> objectResponseHandler(JavaType type) {
   	return new EntityHandler<T>() {
 
 			@Override
 			public T handleEntity(HttpEntity entity) throws IOException {
 				ObjectMapper objectMapper = ObjectMapperProvider.objectMapper();
-				try (InputStream content = entity.getContent()) {
+				try (InputStream content = entity.getContent()) {					
 					if(content instanceof EmptyInputStream) {
 						return null;
 					} else {
-						return objectMapper.readValue(content, type);
+						if(DEBUG) {
+							String text = new String(ByteStreams.toByteArray(content), StandardCharsets.UTF_8);
+							System.out.println(text);
+							return objectMapper.readValue(text, type);
+						} else
+							return objectMapper.readValue(content, type);
 					}
 				}
 			}
